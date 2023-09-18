88f95ec2a4ec63e1daac7e7f9963e975a61563d7
connorbryson5 <cdbryson@yahoo.com>
2023-09-18 13:49:18 -0400

    Adding data to Submission

---
+++ submission/Connor Bryson and Chin-Yu Wu IUPUI Submission/Initial Analysis.R
@@ -0,0 +1,12 @@
+
+# Connor's Working Directory (Desktop)
+setwd("C:/Users/cdbry/Desktop/Sports Analytics/Competitions/2024 Uconn Data Challenge/2024-UCSAS-Data-Challenge/submission/Connor Bryson and Chin-Yu Wu IUPUI Submission")
+
+
+# Loading Libraries
+library(tidyverse)
+
+# Importing Data
+paris_quad <- read_csv(file = "/original data/data_2022_2023.csv")
+
+?read_csv
